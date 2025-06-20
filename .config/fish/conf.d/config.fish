9f2fce6ea4df8fc9b7f05a2df4daf8c53684ad16
Karl Hans Laurits <karl@laurits.dev>
2025-06-20 14:09:27 +0300

    Reorder and update Fish shell config initialization

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,10 +1,10 @@
 if status is-interactive
-    set -g fish_greeting
-    set -g fish_key_bindings fish_vi_key_bindings
+    set --global fish_greeting
+    set --global fish_key_bindings fish_vi_key_bindings
     fish_add_path /opt/homebrew/bin
+    starship init fish | source
+    zoxide init fish --cmd cd | source
     set --global --export SHELL fish
     set --global --export EDITOR hx
     set --global --export PAGER bat
-    starship init fish | source
-    zoxide init fish --cmd cd | source
 end
