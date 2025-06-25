5cc32aa3691f1e400cfec3ec1e45c292c50c9fe9
Karl Hans Laurits <karl@laurits.dev>
2025-06-25 16:18:48 +0300

    Add HashiCorp tap, Helm, libvirt, and Vagrant to Brewfile
    Install vagrant-libvirt plugin in install script

--- install.sh
+++ install.sh
@@ -31,3 +31,5 @@ brew bundle --global check || brew bundle --global
 gh auth status || gh auth login --git-protocol https --hostname github.com --web
 
 gh extension install github/gh-copilot
+
+vagrant plugin install vagrant-libvirt
